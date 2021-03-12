9551fb89b19a79d09302be59794ac22f6812cf36
GanGuiYang <150938420@qq.com>
2021-03-12 09:42:09 +0800

    删除不需要的文件

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/ganguiyang/Downloads/vuepress-starter/node_modules/@vuepress/plugin-back-to-top/lib/components/BackToTop.vue'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/.vuepress/.temp/internal/pagesComponent.js
+++
@@ -1,7 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const pagesComponent = {
-  "/": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"/Users/ganguiyang/Downloads/vuepress-starter/docs/.vuepress/.temp/pages/README.vue")),
-  "/main/": defineAsyncComponent(() => import(/* webpackChunkName: "v-14f0b23d" */"/Users/ganguiyang/Downloads/vuepress-starter/docs/.vuepress/.temp/pages/main/README.vue")),
-  "/404.html": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"/Users/ganguiyang/Downloads/vuepress-starter/docs/.vuepress/.temp/pages/404.html.vue")),
-}
